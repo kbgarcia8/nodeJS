8a35b37286dd71863c23fca19dd529598f3ec045
Karl Garcia <156574512+kbgarcia8@users.noreply.github.com>
2025-05-18 11:23:24 +0000

    introduction to express

--- basic_information_site_express/app.js
+++ basic_information_site_express/app.js
@@ -1,10 +1,28 @@
-import express from "express";
+import { fileURLToPath } from 'url';
+import express from 'express';
+import {dirname} from 'path';
+import path from 'path';
 
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+/* Line 7 and 8 pinpoints the directory where the current code comes from  */
 
-app.get("/", (req, res) => res.send("Hello, world!")); //This what we called as route
+const paths = ['/about', '/contact-me']
+
+app.get(/.*/, (request, response) => { // /.*/ represents wildcard/regex for path
+    const pathname = request.path;
+    if (pathname === '/') {
+        response.sendFile(path.join(__dirname + "/index.html"))
+    } else if (paths.includes(pathname)) {
+        response.sendFile(path.join(__dirname + `${pathname}.html`))
+    } else {
+        response.sendFile(path.join(__dirname + `/404.html`))
+    }
+});
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`My first Express app - listening on port ${PORT}!`);
+    console.log(`Listening on port ${PORT}!`);
 });
