f8a3ed57aab69fec3e2d582ac6424e848a02d4c4
Karl Garcia <156574512+kbgarcia8@users.noreply.github.com>
2025-06-16 08:58:36 +0000

    created repo for next proj

---
+++ inventory_app/app.js
@@ -0,0 +1,36 @@
+import express from "express";
+import path from 'node:path';
+import { fileURLToPath } from 'url';
+import {dirname} from 'path';
+import indexRouter from "./routers/indexRouter.js";
+import usersRouter from "./routers/userRouter.js";
+import messageRouter from "./routers/messageRouter.js";
+
+const app = express();
+const __filename = fileURLToPath(import.meta.url); 
+const __dirname = dirname(__filename);
+const assetsPath = path.join(__dirname, "public");
+
+app.set("views", path.join(__dirname, "views")); 
+app.set("view engine", "ejs");
+
+app.use(express.static(assetsPath));
+
+app.use(express.urlencoded({ extended: true })); //express level middleware to parse the form data into req.body
+app.use(express.json()); //express level middleware to parse json
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/messages", messageRouter);
+
+//error handling using middleware
+app.use((err, req, res, next) => {
+  console.error(err);
+  // We can now specify the `err.statusCode` that exists in our custom error class and if it does not exist it's probably an internal server error
+  res.status(err.statusCode || 500).send(err.message);
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}!`);
+});
