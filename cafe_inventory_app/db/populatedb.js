6ac129cc590d249b68dde05dd64b227974c6dc51
kbg1513 <kbgarcia1513@gmail.com>
2025-06-24 08:44:18 +0000

    table creation

--- cafe_inventory_app/db/populatedb.js
+++ cafe_inventory_app/db/populatedb.js
@@ -15,7 +15,7 @@ CREATE TABLE IF NOT EXISTS message_board.messages (
   id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
   user_id INTEGER REFERENCES message_board.users(id) ON DELETE CASCADE,
   message VARCHAR ( 300 ),
-  dateandtime TEXT DEFAULT TO_CHAR(NOW(), 'YYYY-MM-DD HH12:MI:SS')
+  dateandtime TIMESTAMP DEFAULT NOW()
 );
 
 INSERT INTO message_board.users (username, email)
@@ -47,9 +47,42 @@ VALUES
 `;*/
 
 const MAKETABLES = `
-  
-`;
+  CREATE TABLE IF NOT EXISTS cafe_inventory.users (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    username VARCHAR ( 30 ) NOT NULL UNIQUE,
+    email VARCHAR ( 50 ) NOT NULL,
+    first_name VARCHAR ( 50 ) NOT NULL,
+    last_name VARCHAR ( 50 ) NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW(),
+    photo_url TEXT UNIQUE
+  );
 
+  CREATE TABLE IF NOT EXISTS cafe_inventory.product_categories (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR ( 30 ) NOT NULL UNIQUE
+  );
+
+  CREATE TABLE IF NOT EXISTS cafe_inventory.products (
+    id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    product_code VARCHAR ( 30 ) NOT NULL UNIQUE,
+    name VARCHAR ( 50 ) NOT NULL UNIQUE,
+    price INTEGER NOT NULL,
+    size VARCHAR ( 10 ) NOT NULL,
+    product_category_id INTEGER REFERENCES cafe_inventory.product_categories(id) ON DELETE CASCADE,
+    description TEXT
+  );
+
+  CREATE TABLE IF NOT EXISTS cafe_inventory.cart (
+    user_id INTEGER REFERENCES cafe_inventory.users(id) ON DELETE CASCADE,
+    product_id INTEGER REFERENCES cafe_inventory.products(id) ON DELETE CASCADE,
+    quantity INTEGER NOT NULL,
+    last_updated TIMESTAMP DEFAULT NOW(),
+    PRIMARY KEY (user_id, product_id)
+  );
+
+`;
+//format [TIMESTAMP DEFAULT NOW()] to TO_CHAR(<column_name>, 'YYYY-MM-DD HH12:MI:SS') AS <column_alias>
+//Do this in querying not in defining schema
 
 
 async function main() {
