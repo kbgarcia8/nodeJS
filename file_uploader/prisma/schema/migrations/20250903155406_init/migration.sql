9d8af7758ff5c916b9a7ccf1a5f6c864b725ca78
Karl Garcia <156574512+kbgarcia8@users.noreply.github.com>
2025-09-03 16:16:17 +0000

    Test prismerge and npx prisma migrate successful

---
+++ file_uploader/prisma/schema/migrations/20250903155406_init/migration.sql
@@ -0,0 +1,18 @@
+-- CreateTable
+CREATE TABLE "file_uploader"."User" (
+    "id" SERIAL NOT NULL,
+    "firstName" VARCHAR(50) NOT NULL,
+    "lastName" VARCHAR(100) NOT NULL,
+    "username" VARCHAR(30),
+    "email" VARCHAR(254) NOT NULL,
+    "password" VARCHAR(72) NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "file_uploader"."User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "file_uploader"."User"("email");
